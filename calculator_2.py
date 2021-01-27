a74025df3f594f56048382c87df173f80b332159
София Николаева <NikolaevaSG040@mgpu.ru>
2021-01-27 14:47:45 +0300

    1

--- calculator_2.py
+++ calculator_2.py
@@ -14,69 +14,136 @@ window.show()
 
 
 def one():
-    global first
+    global first, number
+    number = False
     x = "1"
     first += x
+    form.textEdit.setText('%s' %first)
     print(first)
     
 
 def two():
-    global first
+    global first, number
+    number = False
     x = "2"
     first += x
+    form.textEdit.setText('%s' %first)
     print(first)
 
 def three():
-    global first
+    global first, number
+    number = False
     x = "3"
     first += x
+    form.textEdit.setText('%s' %first)
     print(first)
 
 def four():
-    global first
+    global first, number
+    number = False
     x = "4"
     first += x
+    form.textEdit.setText('%s' %first)
     print(first)
 
 def five():
-    global first
+    global first, number
+    number = False
     x = "5"
     first += x
+    form.textEdit.setText('%s' %first)
     print(first)
 
 def six():
-    global first
+    global first, number
+    number = False
     x = "6"
     first += x
+    form.textEdit.setText('%s' %first)
     print(first)
 
 def seven():
-    global first
+    global first, number
+    number = False
     x = "7"
     first += x
+    form.textEdit.setText('%s' %first)
     print(first)
 
 def eight():
-    global first
+    global first, number
+    number = False
     x = "8"
     first += x
+    form.textEdit.setText('%s' %first)
     print(first)
 
 def nine():
-    global first
+    global first, number
+    number = False
     x = "9"
     first += x
+    form.textEdit.setText('%s' %first)
     print(first)
 
 def zero():
-    global first
+    global first, number
+    number = False
     x = "0"
     first += x
+    form.textEdit.setText('%s' %first)
     print(first)
+
+def plus():
+    global first, number
+    if not number:
+        x = "+"
+        first += x
+        form.textEdit.setText('%s' %first)
+        number = True
+    print(first)
+
+def minus():
+    global first, number
+    if not number:
+        x = "-"
+        first += x
+        form.textEdit.setText('%s' %first)
+        number = True
+    print(first)
+
+def multiply():
+    global first, number
+    if not number:
+        x = "*"
+        first += x
+        form.textEdit.setText('%s' %first)
+        number = True
+    print(first)
+
+def division():
+    global first, number
+    if not number:
+        x = "/"
+        first += x
+        form.textEdit.setText('%s' %first)
+        number = True
+    print(first)
+
+def delete():
+    global first, number
+    first = first[:-1]
+    form.textEdit.setText('%s' %first)
+    number = False
+    print(first)
+
+
+   
 
 
 
 first = " "
+number = True
 
 form.pushButton.clicked.connect(one)
 form.pushButton_2.clicked.connect(two)
@@ -88,5 +155,10 @@ form.pushButton_7.clicked.connect(seven)
 form.pushButton_8.clicked.connect(eight)
 form.pushButton_9.clicked.connect(nine)
 form.pushButton_10.clicked.connect(zero)
+form.pushButton_11.clicked.connect(plus)
+form.pushButton_12.clicked.connect(minus)
+form.pushButton_14.clicked.connect(multiply)
+form.pushButton_13.clicked.connect(division)
+form.pushButton_15.clicked.connect(delete)
 
 app.exec_()
